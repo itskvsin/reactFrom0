fb17d5f92f6607e4552e391f011af401021d368d
Kevin <kevinsolankistudy@gmail.com>
2025-03-20 18:59:26 +0530

    prac4-part1

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 
-const title = (
-    <h1 id="heading">Welcome User 👩‍🚀🚀</h1>
-) 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+      </div>
 
-const HeadingComponent = () => (
-  <div>
-    {title}
-    <h1>Welcome To My React Project</h1>
-  </div>
-);
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="resCard"> 
+      <center><h3>Kevinters</h3></center>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="resContainer">
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return ( 
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
+
+
+// timestamp :- 39:09
