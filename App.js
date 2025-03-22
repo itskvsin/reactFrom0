87b6c4baca7e059425194f5687187d964dcee215
Kevin <kevinsolankistudy@gmail.com>
2025-03-22 16:39:49 +0530

    prac4-part2

--- App.js
+++ App.js
@@ -20,10 +20,19 @@ const Header = () => {
   )
 };
 
-const RestaurantCard = () => {
+const restaurentCardStyle = {
+  backgroundColor:'#f0f0f0',
+  color : 'black'
+}
+
+const RestaurantCard = (props) => {
   return (
-    <div className="resCard"> 
-      <center><h3>Kevinters</h3></center>
+    <div className="resCard" style={restaurentCardStyle}> 
+      <center><img className="resImg" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" /></center>
+      <center><h3>{props.resName}</h3></center>
+      <h4>{props.cuisine}</h4>
+      <h4>4.3 Stars</h4>
+      <h4>38 mins</h4>
     </div>
   )
 }
@@ -33,7 +42,8 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="resContainer">
-        <RestaurantCard />
+        <RestaurantCard  resName = 'KevinTers' cuisine='Chai aur Tea'/>
+        <RestaurantCard  resName = 'Jay Bhavani' cuisine='Biryani , North Indian , Asian'/>
       </div>
     </div>
   )
@@ -50,6 +60,3 @@ const AppLayout = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
-
-
-// timestamp :- 39:09
