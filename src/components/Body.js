b6c53099faf8895b52070fa9d1a7cef882aaaae7
Kevin <kevinsolankistudy@gmail.com>
2025-03-30 19:02:47 +0530

    prac6-part1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestaurantCard from "./restaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
 
   //Local State Variable
   const [listOfRestaurants , setListOfRestaurant] = useState(resList);
+
+  useEffect(()=>{
+    fetchData();
+  } 
+  , [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.2961468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+    const json = data.json();
+
+    console.log(json)
+  }
 
   return (
     <div className="body">
