47b8f36034555059d608ff9ba322eed4d40ad1d5
Kevin <kevinsolankistudy@gmail.com>
2025-05-10 11:32:53 +0530

    added a toggle login logout button for better understanding of useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./restaurantCard";
 import resList from "../utils/mockData";
 import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
@@ -21,20 +22,24 @@ const Body = () => {
     setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
   }
 
-  if (listOfRestaurants.length === 0) {
-    return (
-      <div>
-        <h1>Loading....</h1>
-        {/* <video width="320" height="240" controls autoPlay>
-        <source src="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.vecteezy.com/free-videos/loading-gif&ved=2ahUKEwj4ua6d-pONAxW9mq8BHX5qANwQuAJ6BAgQEAU&usg=AOvVaw2fi5uCGN_mSe_0UaiMjXLD" type="gif"/>
-        Your browser does not support the video tag.
-      </video>  */} 
-      {/* A loading screen can be added later */}
-      </div>
-    );
-  }
 
-  return (
+  //Conditional Rendering :- Rendering on condition
+  // if (listOfRestaurants.length === 0) {
+  //   return (
+  //     <div>
+  //       <Shimmer />
+  //       {/* <video width="320" height="240" controls autoPlay>
+  //       <source src="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.vecteezy.com/free-videos/loading-gif&ved=2ahUKEwj4ua6d-pONAxW9mq8BHX5qANwQuAJ6BAgQEAU&usg=AOvVaw2fi5uCGN_mSe_0UaiMjXLD" type="gif"/>
+  //       Your browser does not support the video tag.
+  //     </video>  */} 
+  //     {/* A loading screen can be added later */}
+  //     </div>
+  //   );
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer /> 
+  ) : (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,28 @@
 import {LOGO_URL} from '../utils/constant'
 import image from "../public/logo.png";
+import { useState } from 'react';
 
 const Header = () => {
+  const [btnNameReact , setBtnNameReact] = useState('login');
+
   return (
     <div className="header">
       <div className="logoContainer">
         <img
           className="logo"
           src={image}
-        />
+          />
       </div>
 
       <div className="navItems">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <a href='#'> <li>Home</li> </a> 
+          <a href='#'> <li>About Us</li> </a> 
+          <a href='#'> <li>Contact Us</li> </a> 
+          <a href='#'> <li>Cart</li> </a> 
+          <button onClick={() => {
+            btnNameReact === 'logout' ? setBtnNameReact('login') : setBtnNameReact('logout');
+          }}>{btnNameReact}</button>
         </ul>
       </div>
     </div>
