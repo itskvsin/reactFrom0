fbb49d06f73be3ab3c85ae3c9b2ec7bf5e6c6943
Kevin <kevinsolankistudy@gmail.com>
2025-05-26 18:54:44 +0530

    reinstalled the bundler and tailwind css

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,6 +1,6 @@
 
 const Grocery = () => {
-    return <h1>This is page is for groceries and to see how code splitting / chunking / lazy loading works</h1>
+    return <h1 className="bg-red-500">This is page is for groceries and to see how code splitting / chunking / lazy loading works</h1>
 }
 
 export default Grocery;
