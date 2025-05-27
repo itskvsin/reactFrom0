0ed0d78316f3b9775d8360574a3088047c337730
Kevin <kevinsolankistudy@gmail.com>
2025-05-27 12:14:29 +0530

    repo will be deleted due to kya farak pdta hai delete hih hone wali hai

--- src/components/Grocery.js
+++
@@ -1,6 +0,0 @@
-
-const Grocery = () => {
-    return <h1 className="bg-red-500">This is page is for groceries and to see how code splitting / chunking / lazy loading works</h1>
-}
-
-export default Grocery;
